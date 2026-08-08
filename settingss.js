f17829e4bfcaabe08d753172be06e18941e02e5e
diratos.exe <bobowail123123@gmail.com>
2026-08-08 01:10:15 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  SESSION_ID: "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=d73eeaa4421f02d9",  // add your Session Id here
+  OWNER_NUMBER: "212718398196", // put your phone number here
+  PREFIX: "?", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
